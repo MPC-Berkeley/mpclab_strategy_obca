5780052f3dfbeab760ff63057ca299f708f19839
Edward <zhu.edward445@gmail.com>
2020-10-11 15:30:35 -0700

    Add some skeleton code

--- src/mpclab_strategy_obca/src/mpclab_strategy_obca/state_machine/stateMachine.py
+++ src/mpclab_strategy_obca/src/mpclab_strategy_obca/state_machine/stateMachine.py
@@ -1,6 +1,8 @@
 #!/usr/bin python3
 
+from mpclab_strategy_obca.utils.types import experimentParams
+
 class stateMachine(object):
 
-    def __init__(self):
+    def __init__(self, params=experimentParams()):
         pass
---
+++ src/mpclab_strategy_obca/src/mpclab_strategy_obca/utils/utils.py
@@ -0,0 +1,4 @@
+#!/usr/bin python3
+
+def check_collision_poly(P_1, P_2):
+    pass
